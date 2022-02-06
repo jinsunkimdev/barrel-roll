e3948c8610d0557b12a33eac68fa1036f225446e
jinsunkim <jinsunkim94@gmail.com>
2022-02-06 12:50:40 +0900

    Add api for cocktail info

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { HeaderNav } from "../components/Navbar";
+import axios from "axios";
 
 const Search = () => {
+  const [loading, setLoading] = useState(true);
+  const [cocktail, setCocktails] = useState([]);
+  const getCocktails = async () => {
+    const json = await (
+      await fetch(`${process.env.REACT_APP_COCKTAIL_SEARCH_BY_NAME_KEY}`)
+    ).json();
+    setCocktails(json.drinks);
+  };
+  useEffect(() => {
+    getCocktails();
+    setLoading(false);
+  }, []);
+  console.log(cocktail);
   return (
-    <div>
+    <div className="font-burtons text-comfort-green">
       <HeaderNav />
-      <h1>search page</h1>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div className="">
+          {cocktail.map((cocktail) => (
+            <div key={cocktail.idDrink}>
+              <div className="flex flex-col">
+                <img src={cocktail.strDrinkThumb} className="w-52" alt="" />
+                <h1 className="text-2xl text-comfort-orange">
+                  {cocktail.strDrink}
+                </h1>
+                <ul>
+                  <li>{cocktail.strAlcoholic}</li>
+                  <li>{cocktail.strCategory}</li>
+                </ul>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
